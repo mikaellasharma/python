5478f4ff81fb2d4356af55f89ebe31274f692a3c
mikaellasharma <82779072+mikaellasharma@users.noreply.github.com>
2021-05-26 18:33:23 +0400

    Create ex1.py

---
+++ labs/ex1.py
@@ -0,0 +1,11 @@
+#comment example
+print("Program for examples")
+a=int(input("Please enter a number"))
+b=int(input("Please enter another number"))
+print(a*b)
+print(a/b)
+print(a+b)
+print(a-b)
+print(a//b)
+print(a%2==0 & b%2==0)
+
